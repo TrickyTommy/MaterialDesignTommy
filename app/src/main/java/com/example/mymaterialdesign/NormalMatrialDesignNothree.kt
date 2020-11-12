509c8cbee4c00c72b966569037801b0ef587b86c
Tommy Ferdian Hadimarta <trickytommy4@gmail.com>
2020-11-13 06:26:30 +0700

    wip

--- app/src/main/java/com/example/mymaterialdesign/NormalMatrialDesignNothree.kt
+++ app/src/main/java/com/example/mymaterialdesign/NormalMatrialDesignNothree.kt
@@ -2,10 +2,17 @@ package com.example.mymaterialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+//import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.drawerlayout.widget.DrawerLayout
 
 class NormalMatrialDesignNothree : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_normal_matrial_design_nothree)
-    }
+
 }
