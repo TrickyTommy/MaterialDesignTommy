dec53493d3f3622f5fb8b8326f98b8b04b0453ef
TrickyTommy <trickytommy4@gmail.com>
2020-11-13 09:40:39 +0700

    wip

--- app/src/main/java/com/example/mymaterialdesign/adapter/AdapterProduct.kt
+++ app/src/main/java/com/example/mymaterialdesign/adapter/AdapterProduct.kt
@@ -1,17 +1,21 @@
 package com.example.mymaterialdesign.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mymaterialdesign.model.ProductModel
 
-class AdapterProduct (private val context: Context, private val items: List<ProductModel>): RecyclerView.Adapter<AdapterProduct.ProductViewHolder>(){
+class AdapterProduct (private val context: Context, private val items: List<ProductModel>, private val  itemListener: PostItemListener): RecyclerView.Adapter<AdapterProduct.ProductViewHolder>(){
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): AdapterProduct.ProductViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(context)
+        val view: View = inflater.inflate(R.layout.third_item, parent, false)
+        return ProductViewHolder(view, this.itemListener)
     }
 
     override fun getItemCount(): Int {
