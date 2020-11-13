22abf5073bd37cc49c4a72429f674d8dc4497ab7
TrickyTommy <trickytommy4@gmail.com>
2020-11-13 11:18:26 +0700

    wip

--- app/src/main/java/com/example/mymaterialdesign/adapter/AdapterProduct.kt
+++ app/src/main/java/com/example/mymaterialdesign/adapter/AdapterProduct.kt
@@ -1,31 +1,53 @@
 package com.example.mymaterialdesign.adapter
 
+import  com.example.mymaterialdesign.model.ProductModel
+
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.mymaterialdesign.model.ProductModel
+import com.bumptech.glide.Glide
+import com.example.mymaterialdesign.databinding.ItemProductBinding
 
-class AdapterProduct (private val context: Context, private val items: List<ProductModel>, private val  itemListener: PostItemListener): RecyclerView.Adapter<AdapterProduct.ProductViewHolder>(){
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): AdapterProduct.ProductViewHolder {
-        val inflater = LayoutInflater.from(context)
-        val view: View = inflater.inflate(R.layout.third_item, parent, false)
-        return ProductViewHolder(view, this.itemListener)
+
+class AdapterProduct(private val context: Context):RecyclerView.Adapter<AdapterProduct.ViewHolder>(){
+
+    private var products = listOf<ProductModel>()
+
+    fun setData(data: List<ProductModel>) {
+        products = data
+        notifyDataSetChanged()
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    inner class ViewHolder(private val context: Context, private val binding:ItemProductBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun binData(product: ProductModel) {
+            binding.tvProduct.text = product.title
+            binding.tvPrice.text = "$${product.price}"
+            Glide.with(binding.root).load(product.imageUrl).into(binding.ivProduct)
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterProduct.ViewHolder {
+        return ViewHolder(
+            ItemProductBinding.inflate(
+                LayoutInflater.from(context),
+                parent,
+                false
+            )
+        )
     }
 
-    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-    inner class ProductViewHolder {
 
+    override fun onBindViewHolder(holder: AdapterProduct.ViewHolder, position: Int) {
+        holder.binData(products[position])
     }
-}
+
+    override fun getItemCount(): Int = products.size
+    }
+
+
+
+
+
+
